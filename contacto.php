66f48fab0636e4c61cf1ff84a4499cb99092f555
Arnaldo Jose Bonillo Berrios <NM41126@banvenez.corp>
2022-09-01 14:48:43 -0430

    Cambio en la seccion el equipo se quitaron las fotos

--- contacto.php
+++ contacto.php
@@ -5,8 +5,9 @@ $nom = $_POST['name'];
 $cor = $_POST['email'];
 $asu = $_POST['subject'];
 $men = $_POST['message'];
+$tel = $_POST['phone'];
 
-$content = "Nombre: ".$nom . "<br>" . "\nEmail: ".$cor ."<br>" . "\Asunto: ". $asu . "<br>" . "\n             Mensaje: ".$men;
+$content = "Nombre: ".$nom . "<br>" . "\nEmail: ".$cor ."<br>" . "\Asunto: ". $asu . "<br>" . "\n Mensaje: ". $men . "<br>" . "\n Telefono: ". $tel;
 
 
 mail($des, $asu, $content);
